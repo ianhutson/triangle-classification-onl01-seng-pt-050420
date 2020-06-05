f95dd276d172a2537d3ec6c43b366900bf24b969
Ian Hutson <ih00209@georgiasouthern.edu>
2020-06-05 20:49:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,7 +22,7 @@ def triangle
   tri = [(a + b > c), (a + c > b), (b + c > a)]
   [a, b, c].each do |side|
       real_triangle << false if side <= 0 
-    raise TriangleError if real_triangle.include?(false)
+    raise TriangleError if tri.include?(false)
     end
   end
 
